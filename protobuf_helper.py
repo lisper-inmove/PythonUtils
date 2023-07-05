79a4b4c259e32b96d90b0bae7574f58c91a9a715
inmove <lisper.inmove@gmail.com>
2023-07-05 16:50:56 +0800

    Add to_json_v2, to_obj_v2

--- protobuf_helper.py
+++ protobuf_helper.py
@@ -20,6 +20,24 @@ class ProtobufHelper:
 
     @staticmethod
     def to_json(data):
+        """deprecated, use to_dict instead"""
+        if data is None:
+            return None
+        if isinstance(data, str):
+            return data
+        if isinstance(data, list):
+            return data
+        if isinstance(data, dict):
+            return data
+        if isinstance(data, bool):
+            return data
+        return json_format.MessageToDict(
+            data, including_default_value_fields=True,
+            preserving_proto_field_name=True
+        )
+
+    @staticmethod
+    def to_dict(data):
         if data is None:
             return None
         if isinstance(data, str):
@@ -40,3 +58,16 @@ class ProtobufHelper:
         if not data:
             return []
         return [ProtobufHelper.to_json(d) for d in data]
+
+    @staticmethod
+    def to_json_v2(msg):
+        return json_format.MessageToJson(
+            msg,
+            including_default_value_fields=True,
+            preserving_proto_field_name=True)
+
+    @staticmethod
+    def to_obj_v2(data, cls):
+        if data is None:
+            return None
+        return json_format.Parse(data, cls(), ignore_unknown_fields=True)
