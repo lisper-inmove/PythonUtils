f53d0d974d240cc0482abcb4c04face5c72b9646
inmove <lisper.inmove@gmail.com>
2024-03-20 21:25:38 +0800

    add pypdf

--- pdf_util.py
+++ pdf_util.py
@@ -1,5 +1,9 @@
+from asyncio import current_task
 import aiohttp
 import fitz
+import PyPDF2
+from fitz.mupdf import pdf_new_uri_from_path_and_named_dest
+from pypdf import PdfReader
 
 async def download_pdf_async(url: str, filename: str):
     async with aiohttp.ClientSession() as session:
@@ -19,7 +23,44 @@ async def download_pdf_async(url: str, filename: str):
 
 def read_pdf_sync(pdf_path: str):
     contents = []
+    curContent = ""
     with fitz.open(pdf_path) as doc:
         for page in doc:
-            contents.append(page.get_text())
+            curContent += page.get_text()
+            if len(curContent) >= 2000:
+                contents.append(curContent)
+                curContent = ""
     return contents
+
+def read_pdf_sync_v2(pdf_path: str):
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        for page in reader.pages:
+            print(page)
+
+def read_pdf_sync_v3(pdf_path: str):
+    reader = PdfReader(pdf_path)
+    contents = []
+    curContent = ""
+    for page in reader.pages:
+        curContent += page.extract_text()
+        if len(curContent) >= 2000:
+            contents.append(curContent)
+            curContent = ""
+    return contents
+
+
+if __name__ == "__main__":
+    import asyncio
+    async def main():
+        url = "https://utfs.io/f/c6ed7fde-e0bf-4d7a-a327-09c5f8483423-125zs.pdf"
+        filename = "test"
+        filepath = await download_pdf_async(url, filename)
+        contents = read_pdf_sync_v3(filepath)
+        __import__('pdb').set_trace()
+        print(123)
+
+    server = main()
+    asyncio.get_event_loop().run_until_complete(server)
+    asyncio.get_event_loop().run_forever()
+
