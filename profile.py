e72e0062ffbfee78925002fa6576583a26114c43
inmove <lisper.inmove@gmail.com>
2024-03-22 11:37:33 +0800

    add func time expend for async functions

--- profile.py
+++ profile.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
+
 from functools import wraps
 
 from .logger import Logger
@@ -24,6 +26,33 @@ class FuncTimeExpend(TimeExpand):
             end_time_sec_msec = IDate.now_milliseconds()
             logger.info(f"{self._prefix} {fn.__name__} consume {end_time_sec_msec - start_time_msec} milliseconds")
             if end_time_sec_msec - start_time_msec > 0.5:
-                logger.info(f"{self._prefix} {fn.__name__} params {args} {kargs}")
+                logger.warning(f"{self._prefix} {fn.__name__} params {args} {kargs}")
             return result
         return inner
+
+
+def func_time_expend_async(fn):
+    @wraps(fn)
+    async def wrapper_async_function(*args, **kargs):
+        start_time_msec = IDate.now_milliseconds()
+        result = await fn(*args, **kargs)
+        end_time_sec_msec = IDate.now_milliseconds()
+        logger.info(f"{fn.__name__} consume {end_time_sec_msec - start_time_msec} milliseconds")
+        if end_time_sec_msec - start_time_msec > 0.5:
+            logger.warning(f"{fn.__name__} params {args} {kargs}")
+        return result
+
+    @wraps(fn)
+    async def wrapper_async_generator(*args, **kargs):
+        start_time_msec = IDate.now_milliseconds()
+        async for value in fn(*args, **kargs):
+            yield value
+        end_time_sec_msec = IDate.now_milliseconds()
+        logger.info(f"{fn.__name__} consume {end_time_sec_msec - start_time_msec} milliseconds")
+        if end_time_sec_msec - start_time_msec > 0.5:
+            logger.warning(f"{fn.__name__} params {args} {kargs}")
+
+    if asyncio.iscoroutinefunction(fn):
+        return wrapper_async_function
+    else:
+        return wrapper_async_generator
